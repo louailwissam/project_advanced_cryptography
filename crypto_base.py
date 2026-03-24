a105b87be83c33b5c5e039179febab40ac5151b6
louailwissam <Louailwissam0105@gmail.com>
2026-03-24 12:01:01 +0100

    Ajout de la classe abstraite de base pour les algorithmes

---
+++ crypto_base.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+
+class AlgorithmeCryptographique(ABC):
+    """
+    Classe abstraite de base pour tous les algorithmes de cryptographie.
+    Chaque algorithme (César, AES, DES, etc.) devra hériter de cette classe
+    et implémenter obligatoirement ces deux méthodes.
+    """
+
+    @abstractmethod
+    def chiffrer(self, texte_clair, cle):
+        """
+        Chiffre un message en clair avec une clé donnée.
+        """
+        pass
+
+    @abstractmethod
+    def dechiffrer(self, texte_chiffre, cle):
+        """
+        Déchiffre un message chiffré avec une clé donnée.
+        """
+        pass
